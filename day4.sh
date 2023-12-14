40105c888531952554476d5e6b8b95d3cc463a99
gaurav srivastava <gaurav.srivastava@enine.school>
2023-12-14 15:36:57 +0530

    making bash file

---
+++ day4.sh
@@ -0,0 +1,20 @@
+echo "below are the commands i have used"
+cd 
+cd Documents/
+touch notes.txt
+nano notes.txt 
+cd ..
+cd Downloads/
+ls| grep .pdf
+cd ..
+cd Documents/
+mkdir backup
+cp notes.txt backup/
+cp notes.txt backup/
+chmod +rw notes.py
+grep -rl "important" ~/
+cd Documents
+rm -r backup ps
+ps -e
+mv notes.txt new_notes.txt
+echo "Thank you for teaching!!"
